1417ac662ded4e5b6605d85af84803fe728ca917
ayoubelrhanni <ayoub.elrhanni@hotmail.fr>
2024-04-30 10:01:28 +0100

    navbar

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -1,15 +1,30 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/Device"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
 
-        return Controller.extend("gestiondispo.controller.Dashboard", {
-            onInit: function () {
+    return Controller.extend("gestiondispo.controller.Dashboard", {
+      onInit: function () {},
+      onAvatarPressed: function () {
+        console.log("Avatar pressed!");
+      },
+      onLogoPressed: function () {
+        MessageToast.show("Logo pressed!");
+      },
+      onCollapseExpandPress() {
+        const oSideNavigation = this.byId("sideNavigation"),
+            bExpanded = oSideNavigation.getExpanded();
 
-            }
-        });
+        oSideNavigation.setExpanded(!bExpanded);
+    },
+
+    onHideShowWalkedPress() {
+        const oNavListItem = this.byId("walked");
+        oNavListItem.setVisible(!oNavListItem.getVisible());
+    }
     });
+  }
+);
